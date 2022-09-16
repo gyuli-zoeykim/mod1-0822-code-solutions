a42ad06f1468d2b3e1e64846889347270be57e51
Zoey1318 <gyu4485@gmail.com>
2022-09-16 03:35:07 +0000

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,54 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    delivered: 'Aug 8, 2020',
+    imageUrl: '/images/jsbook.jpg',
+    deliverHow: '',
+    productName: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr.Axel',
+    returnWindowClosedOn: 'Sep 7, 2020',
+    subTotal: '$31.55'
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    delivered: 'Jul 20, 2020',
+    imageUrl: '/images/buildingbook.jpg',
+    deliverHow: 'Your package was delivered. It was handed directly to a resident',
+    productName: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    returnWindowClosedOn: 'Aug 19, 2020',
+    subTotal: '$41.33'
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    delivered: 'Jul 7, 2020',
+    imageUrl: '/images/adapter.jpg',
+    deliverHow: 'Your package was delivered. It was handed directly to a resident',
+    productName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wll U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    author: '',
+    returnWindowClosedOn: 'Aug 5, 2020',
+    subTotal: '$15.98'
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    delivered: 'Jul 5, 2020',
+    imageUrl: ['/images/controller.jpg, /images/sqlbook.jpg'],
+    deliverHow: '',
+    productName: ['GameCube Controller - Super Smash Bros. Edition (Nitendo Switch)', 'The Art of Sql'],
+    author: '',
+    returnWindowClosedOn: ['Aug 4, 2020', 'Aug 4, 2020'],
+    subTotal: ['$94.95', '$33.99']
+  }
+];
