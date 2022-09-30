c1f2f90629d3fa7f71ee3f1d4ba5b48429c0c184
Zoey1318 <gyu4485@gmail.com>
2022-09-30 03:40:09 +0000

    Completed object-manipulation-1

--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -1,7 +1,6 @@
 /* exported setValue */
 // 1. define a function with three parameter
 // 2. use object accessor to add value in existing object property
-// 3. return value
 
 function setValue(object, key, value) {
   var objValue = object;
