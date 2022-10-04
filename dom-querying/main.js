d1124004755e78e40e71290bb209b8bc722052f0
Zoey1318 <gyu4485@gmail.com>
2022-10-04 03:22:12 +0000

    fixed commented code

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,18 +1,22 @@
 console.log('hello, world');
 
 document.querySelector('h1');
-console.log(document.querySelector('h1'));
-console.dir('h1');
+var $heading = document.querySelector('h1');
+console.log('$heading', $heading);
+console.dir($heading);
 
-document.querySelector('p#explanation');
-console.log(document.querySelector('p#explanation'));
+document.querySelector('#explanation');
+var $explaination = document.querySelector('#explanation');
+console.log('$explaination', $explaination);
 console.dir('p#explanation');
 
-document.querySelector('p.hint');
-console.dir(document.querySelector('p.hint'));
+document.querySelector('.hint');
+var $hint = document.querySelector('.hint');
+console.log('$hint', $hint);
+console.dir('p.hint');
 
 document.querySelectorAll('p');
 console.log(document.querySelectorAll('p'));
 
-document.querySelectorAll('a.example-link');
-console.log(document.querySelectorAll('a.example-link'));
+document.querySelectorAll('.example-link');
+console.log(document.querySelectorAll('.example-link'));
