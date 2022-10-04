19dbd02c1a8a710246275286b560e74344747fec
Zoey1318 <gyu4485@gmail.com>
2022-10-04 02:26:40 +0000

    Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('hello, world');
+
+document.querySelector('h1');
+console.log(document.querySelector('h1'));
+console.dir('h1');
+
+document.querySelector('p#explanation');
+console.log(document.querySelector('p#explanation'));
+console.dir('p#explanation');
+
+document.querySelector('p.hint');
+console.dir(document.querySelector('p.hint'));
+
+document.querySelectorAll('p');
+console.log(document.querySelectorAll('p'));
+
+document.querySelectorAll('a.example-link');
+console.log(document.querySelectorAll('a.example-link'));
